594c91d5e46808f481047fd9fefb6aee1f256fb3
Praveen Gadikoppula <praveen.androids@gmail.com>
2017-09-07 16:05:57 +0530

    modified

--- itikyal/Vulnerawa1.0.2/menu.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo '<h1><a href="action.php">Login</a>
-<a href="about.php">About</a></h1>'
-?>
--- itikyal/Vulnerawa1.0.2/xx.php
+++
@@ -1,9 +0,0 @@
-
-<!DOCTYPE html>
-<html> <body><?php
-if($_COOKIE)
-{   print_r($_COOKIE);
-//print all cookie
-}else {    
-echo "COOKIE is not set"; 
-?></body></html>
