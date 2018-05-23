65a90db971a191f4c9dbf9bb6d09e844745fb649
Praveen Gadikoppula <praveen.androids@gmail.com>
2018-05-24 00:51:34 +0530

    db changed to online

--- config.sql
+++ config.sql
@@ -1,11 +1,10 @@
 <?php
 
-$con=mysqli_connect("localhost","root","1234","govt");
+$con=mysqli_connect("sql313.cuccfree.com","cucch_14999267","p1p2p364","cucch_14999267_itikyal");
 
 if (mysqli_connect_errno())
 
-{ echo "praveen i am sorry mysql not connect:".mysqli_connect_error();
-
+{ echo " sorry praveen mysql not connected :".mysqli_connect_error();
 } 
 
 ?>
